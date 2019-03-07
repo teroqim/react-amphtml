d2f7f296057351dbdc27c62b0de51f59da22d577
teroqim <teroqim@gmail.com>
2019-03-07 19:35:27 +0100

    Bump amp-mustache version

--- src/amphtml/components/Script.js
+++ src/amphtml/components/Script.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const versionForExtension = (extension) => {
+  switch (extension) {
+    case 'amp-mustache':
+      return '0.2';
+    default:
+      return '0.1';
+  }
+}
+
 const Script = ({ extension, isCustomTemplate }) => (
   <script
     async
     {...{ [`custom-${isCustomTemplate ? 'template' : 'element'}`]: extension }}
-    src={`https://cdn.ampproject.org/v0/${extension}-0.1.js`}
+    src={`https://cdn.ampproject.org/v0/${extension}-${versionForExtension(extension)}.js`}
   />
 );
 
